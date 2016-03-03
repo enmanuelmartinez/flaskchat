d412b0e0f404827cc448811ddf583b64cc4818aa
Enmanuel Martinez <enmanuel.martinez.g@gmail.com>
2016-03-03 17:05:33 -0400

    Socket io initialize in app, bower.json

--- app.py
+++ app.py
@@ -3,18 +3,23 @@
 from flask import Flask, render_template, session, redirect, url_for, escape, request
 from flask.ext.socketio import SocketIO, emit
 
-app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'secret!'
-#socketio = SocketIO(app)
+app = Flask(__name__, static_url_path='/static')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return "login"
+    if request.method is 'POST':
+        return 'login POST'
+    elif request.method is 'GET':
+	return 'login GET'
+    
+    return render_template('login.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
